5da045119118aae17f7b4f9d6821a02d59426a97
ZhEgor <41516868+ZhEgor@users.noreply.github.com>
2018-07-24 22:41:25 +0300

    Update JS.js

--- JS.js
+++ JS.js
@@ -1,5 +1,5 @@
 function Play(){
-	var	h,m,s,ms,S,t,V,t_l,S_l,l,out;
+	var	h,m,s,ms,S,t,V,t_l,S_l,l,out,out1;
 	S = document.getElementById("distance").value;
 	l = document.getElementById("Lines").value;
 	h = document.getElementById("hour").value;		
@@ -7,16 +7,16 @@ function Play(){
 	s = document.getElementById("second").value;
 	ms = document.getElementById("millisecond").value;
 	out = document.getElementById("out");
+//	out1 = document.getElementById("out1");
 	h = parseInt(h);
 	m = parseInt(m);
 	s = parseInt(s);
 	ms = parseInt(ms);
-	
+	out.innerHTML='';
+
 	t = (h * 60 * 60 * 1000) + (m * 60 * 1000) + (s * 1000) + ms;
 	V = S/t;
 	S_l=S/l;
-	var m_t,s_t,ms_t;
-//	m_t=0;s_t=0;ms_t=0;
 
 	for(var i = 1; i<=S_l;++i)
 	{	
@@ -31,18 +31,17 @@ function Play(){
 		ms = parseInt(ms);
 		while(m>=60)
 		{
-		m-=60;		
+			m-=60;		
 		}
 		while(s>=60)
 		{
-		s-=60;		
+			s-=60;		
 		}
 		while(ms>=1000)
 		{
-		ms-=1000;		
+			ms-=1000;		
 		}
-			
-		out.innerHTML += i*l+'м - '+h+':'+m+':'+s+':'+ms+"<br>";	
+		out.innerHTML += i*l+'м - '+h+':'+m+':'+s+':'+ms+"\n";
+		
 	}
-
 }
